279baf48a4947326da1ecb36c84eaf4e5a4bbbbf
WJ-sr <wonjin.kim@seoulrobotics.org>
2020-11-20 07:39:30 +0900

    created pyqt launching function

---
+++ main_app.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QApplication
+import sys
+
+class App(QApplication):
+    def __init__(self, sys_argv):
+        super(App, self). __init__(sys_argv) 
+        print("Hello World")
+
+
+
+if __name__ == "__main__":
+    app = App(sys.argv)
+    sys.exit(app.exec_()) 
